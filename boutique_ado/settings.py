80a390173a6bf8bdc05adf005b335148167b8f38
LNoonan3 <lucynoonan3@gmail.com>
2025-05-06 15:52:43 +0100

    fix the setting.py emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -232,6 +232,3 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
-
-
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
